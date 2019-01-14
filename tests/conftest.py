b3361de1d0f42c539be07e09ca875602dd267ad2
Takeshi KOMIYA <i.tkomiya@gmail.com>
2019-01-14 13:50:50 +0900

    SPHINX_TEST_TEMPDIR is no longer needed for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,9 +6,6 @@
     :license: BSD, see LICENSE for details.
 """
 
-import os
-import shutil
-
 import pytest
 
 from sphinx.testing.path import path
@@ -18,22 +15,4 @@ pytest_plugins = 'sphinx.testing.fixtures'
 
 @pytest.fixture(scope='session')
 def rootdir():
-    return path(os.path.dirname(__file__)).abspath() / 'roots'
-
-
-def _initialize_test_directory(session):
-    testroot = os.path.join(str(session.config.rootdir), 'tests')
-    tempdir = os.path.abspath(os.getenv('SPHINX_TEST_TEMPDIR',
-                              os.path.join(testroot, 'build')))
-    os.environ['SPHINX_TEST_TEMPDIR'] = tempdir
-
-    print('Temporary files will be placed in %s.' % tempdir)
-
-    if os.path.exists(tempdir):
-        shutil.rmtree(tempdir)
-
-    os.makedirs(tempdir)
-
-
-def pytest_sessionstart(session):
-    _initialize_test_directory(session)
+    return path(__file__).parent.abspath() / 'roots'
